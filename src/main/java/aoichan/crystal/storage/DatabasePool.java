d316a81dfc61255fea30fae0b48d6ad89fa6f800
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 19:35:45 +0700

    Update DatabasePool.java

--- src/main/java/aoichan/crystal/storage/DatabasePool.java
+++ src/main/java/aoichan/crystal/storage/DatabasePool.java
@@ -1,1 +1,37 @@
+package aoichan.crystal.storage;
 
+import aoichan.crystal.AoiMain;
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+import javax.sql.DataSource;
+
+public class DatabasePool {
+
+    private final HikariDataSource dataSource;
+
+    public DatabasePool(AoiMain plugin) {
+
+        HikariConfig config = new HikariConfig();
+
+        config.setJdbcUrl("jdbc:sqlite:" +
+                plugin.getDataFolder() + "/data.db");
+
+        config.setMaximumPoolSize(10);
+        config.setPoolName("GemsPool");
+
+        dataSource = new HikariDataSource(config);
+    }
+
+    public StorageProvider createProvider() {
+        return new SQLiteStorage(dataSource);
+    }
+
+    public DataSource getDataSource() {
+        return dataSource;
+    }
+
+    public void shutdown() {
+        dataSource.close();
+    }
+}
