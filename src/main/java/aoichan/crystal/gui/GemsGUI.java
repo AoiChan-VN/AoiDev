c43e1d106978a77089187e4db01c22c666a933e2
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-01 19:04:13 +0700

    Update GemsGUI.java

--- src/main/java/aoichan/crystal/gui/GemsGUI.java
+++ src/main/java/aoichan/crystal/gui/GemsGUI.java
@@ -1,1 +1,18 @@
+package aoichan.crystal.gui;
 
+import aoichan.crystal.AoiMain;
+import org.bukkit.Bukkit;
+import org.bukkit.inventory.Inventory;
+
+public class GemsGUI {
+
+    public static Inventory create() {
+        Inventory inv = Bukkit.createInventory(null, 27, "§8Gems Ultimate");
+
+        AoiMain.get().getAPI().getAllGems().forEach(id -> {
+            inv.addItem(AoiMain.get().getAPI().createGemItem(id));
+        });
+
+        return inv;
+    }
+}
