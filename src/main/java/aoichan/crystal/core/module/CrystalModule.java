5f9ee55593c73b6b89d9e2b6ce929cd3bb3995a9
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-09 23:37:46 +0700

    Update and rename .gitkeep to CrystalModule.java

---
+++ src/main/java/aoichan/crystal/core/module/CrystalModule.java
@@ -0,0 +1,13 @@
+package aiochan.crystal.core.module;
+
+public interface CrystalModule {
+
+    // [!] Code: Module Enable
+    void enable();
+
+    // [!] Code: Module Disable
+    void disable();
+
+    // [!] Code: Module Name
+    String name();
+}
