3c334bd85d21bc4f7ef61ccb7659683a5d8bd80e
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-09 23:40:47 +0700

    Update and rename .gitkerp to ConfigLoader.java

---
+++ src/main/java/aoichan/crystal/infrastructure/config/ConfigLoader.java
@@ -0,0 +1,35 @@
+package aiochan.crystal.infrastructure.config;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ConfigLoader {
+
+    private final JavaPlugin plugin;
+
+    private FileConfiguration config;
+
+    public ConfigLoader(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    // [!] Code: Load config.yml
+    public void load() {
+
+        plugin.saveDefaultConfig();
+        plugin.reloadConfig();
+
+        config = plugin.getConfig();
+    }
+
+    // [!] Code: Reload configs
+    public void reload() {
+
+        plugin.reloadConfig();
+        config = plugin.getConfig();
+    }
+
+    public FileConfiguration config() {
+        return config;
+    }
+}
