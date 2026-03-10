c3b2b6f09b5f6888f06969f34ab412defa5e15a8
💋𝓐𝓸𝓲 𝓒𝒽𝒶𝓃🍀𝓼𝓮𝓷𝒾𝓸𝓇🌸 <121602736+AoiChan-Dev@users.noreply.github.com>
2026-03-10 16:36:29 +0700

    Create GiveCommand.java

---
+++ src/main/java/aoichan/crystal/platform/command/sub/GiveCommand.java
@@ -0,0 +1,33 @@
+package aoichan.crystal.platform.command.sub;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class GiveCommand implements SubCommand {
+
+    @Override
+    public void execute(CommandSender sender, String[] args) {
+
+        if (args.length < 4) {
+
+            sender.sendMessage("/crystal give <player> <gem> <level>");
+            return;
+        }
+
+        Player target = Bukkit.getPlayer(args[1]);
+
+        if (target == null) {
+
+            sender.sendMessage("Player not found.");
+            return;
+        }
+
+        String gemId = args[2];
+        int level = Integer.parseInt(args[3]);
+
+        // [!] Code: TODO create gem item
+
+        sender.sendMessage("Gem given.");
+    }
+} 
