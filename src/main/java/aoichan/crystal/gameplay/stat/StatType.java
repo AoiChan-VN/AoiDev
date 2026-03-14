9de9e38f4e3172926e63abf3f21cb92798ed2476
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-15 01:21:06 +0700

    Create StatType.java

---
+++ src/main/java/aoichan/crystal/gameplay/stat/StatType.java
@@ -0,0 +1,22 @@
+package aoichan.crystal.gameplay.stat;
+
+public enum StatType {
+
+    DAMAGE,
+    CRIT_RATE,
+    CRIT_DAMAGE,
+
+    DEFENSE,
+
+    MAX_HEALTH,
+    MAX_MANA,
+
+    MANA_REGEN,
+    LIFE_STEAL,
+
+    SKILL_DAMAGE,
+
+    ATTACK_SPEED
+
+}
+ 
