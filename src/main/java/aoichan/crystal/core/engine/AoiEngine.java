9fad7f314068b89a2c9b679c2ed2b31b819ac4ad
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-14 11:00:35 +0700

    Update and rename .gitkeep to AoiEngine.java

---
+++ src/main/java/aoichan/crystal/core/engine/AoiEngine.java
@@ -0,0 +1,30 @@
+package aoichan.crystal.core.engine;
+
+import aoichan.crystal.infrastructure.config.ConfigManager;
+import aoichan.crystal.infrastructure.util.LogUtil;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class CrystalEngine {
+
+    private final JavaPlugin plugin;
+
+    public CrystalEngine(JavaPlugin plugin) {
+        this.plugin = plugin;
+    }
+
+    public void start() {
+
+        // 【!】Code: Load config
+        ConfigManager.load(plugin);
+
+        LogUtil.info("Crystal Engine Bật");
+
+    }
+
+    public void shutdown() {
+
+        LogUtil.info("Crystal Engine Tắt");
+
+    }
+
+}
