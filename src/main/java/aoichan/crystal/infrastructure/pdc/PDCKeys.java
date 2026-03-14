e5efd477488a98969b0c0c1551f1a42ed1091682
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-14 19:46:53 +0700

    Update PDCKeys.java

--- src/main/java/aoichan/crystal/infrastructure/pdc/PDCKeys.java
+++ src/main/java/aoichan/crystal/infrastructure/pdc/PDCKeys.java
@@ -1,30 +1,27 @@
 package aoichan.crystal.infrastructure.pdc;
 
-import aoichan.crystal.bootstrap.CrystalPlugin;
+import aoichan.crystal.bootstrap.AoiMain;
 import org.bukkit.NamespacedKey;
 
 public class PDCKeys {
 
     // [!] Code: Gem JSON storage
     public static final NamespacedKey GEMS =
-            new NamespacedKey(CrystalPlugin.get(), "crystal_gems");
+            new NamespacedKey(AoiMain.get(), "crystal_gems");
 
     // [!] Code: Socket amount
     public static final NamespacedKey SOCKETS =
-            new NamespacedKey(CrystalPlugin.get(), "crystal_sockets");
+            new NamespacedKey(AoiMain.get(), "crystal_sockets");
 
     // [!] Code: Gem name id
     public static final NamespacedKey GEM_ID =
-        new NamespacedKey(CrystalPlugin.get(), "gem_id");
+        new NamespacedKey(AoiMain.get(), "gem_id");
 
     // [!] Code: Gem level
     public static final NamespacedKey GEM_LEVEL =
-        new NamespacedKey(CrystalPlugin.get(), "gem_level");
+        new NamespacedKey(AoiMain.get(), "gem_level");
 
     // [!] Code:
     public static final NamespacedKey CHARM_ID =
-        new NamespacedKey(
-                CrystalPlugin.get(),
-                "charm_id"
-        );
+        new NamespacedKey(AoiMain.get(), "charm_id");
 } 
