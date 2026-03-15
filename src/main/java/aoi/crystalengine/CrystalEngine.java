6bd263ada9b8a6452890ed11b854a604e74acffa
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-15 09:04:31 +0700

    Update CrystalEngine.java

--- src/main/java/aoi/crystalengine/CrystalEngine.java
+++ src/main/java/aoi/crystalengine/CrystalEngine.java
@@ -1,1 +1,62 @@
+package com.aoi.crystalengine;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import com.aoi.crystalengine.player.PlayerManager;
+import com.aoi.crystalengine.storage.DataStorage;
+import com.aoi.crystalengine.api.service.ServiceManager;
+import com.aoi.crystalengine.util.Log;
+
+/*
+#【!】Code:
+Main plugin class.
+Đây là lõi khởi động của toàn bộ hệ thống Crystal.
+Các plugin khác sẽ hook vào Engine thông qua API.
+*/
+
+public class CrystalEngine extends JavaPlugin {
+
+    private static CrystalEngine instance;
+
+    private PlayerManager playerManager;
+    private DataStorage dataStorage;
+    private ServiceManager serviceManager;
+
+    @Override
+    public void onEnable() {
+
+        instance = this;
+
+        Log.info("Booting CrystalEngine...");
+
+        // init systems
+        serviceManager = new ServiceManager();
+        dataStorage = new DataStorage();
+        playerManager = new PlayerManager();
+
+        Log.success("CrystalEngine enabled successfully.");
+    }
+
+    @Override
+    public void onDisable() {
+
+        Log.info("CrystalEngine shutting down...");
+    }
+
+    public static CrystalEngine get() {
+        return instance;
+    }
+
+    public PlayerManager getPlayerManager() {
+        return playerManager;
+    }
+
+    public DataStorage getDataStorage() {
+        return dataStorage;
+    }
+
+    public ServiceManager getServiceManager() {
+        return serviceManager;
+    }
+}
 
