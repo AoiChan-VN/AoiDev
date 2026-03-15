284e54e49f95550110272f110d26ce0615bb06ec
🍀𝓐𝓸𝓲 𝓒𝒽𝒶𝓃💋 <121602736+AoiChan-VN@users.noreply.github.com>
2026-03-15 10:38:04 +0700

    Create CrystalAPI.java

---
+++ src/main/java/aoi/crystalengine/api/CrystalAPI.java
@@ -0,0 +1,17 @@
+package com.aoi.crystalengine.api;
+
+import com.aoi.crystalengine.CrystalEngine;
+
+/*
+#【!】Code:
+Public API cho plugin khác
+*/
+
+public class CrystalAPI {
+
+    public static CrystalEngine engine() {
+
+        return CrystalEngine.get();
+    }
+
+} 
